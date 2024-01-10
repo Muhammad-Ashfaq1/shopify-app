b51eadc0ca39aefef4c53ca8bc2b4cf5b4151db7
Pak <pak@Paks-MacBook-Pro.local>
2024-01-11 02:17:39 +0500

    testing route

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->middleware(['verify.shopify'])->name('home');
+
+
+Route::get('/testing123', function () {
+    return 'testing123';
+});
